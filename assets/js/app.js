0162764ff163b552970a4e6cd555b0acc74fc95a
rizalkholifnurrohman <rizalhellboy@gmail.com>
2022-07-07 22:33:33 +0700

    updates: - add landing page - create and styling some components - create sidebar - create navbar

---
+++ assets/js/app.js
@@ -0,0 +1,34 @@
+(function (window, $) {
+  $(function () {
+    /* nav dropdown */
+    let navLink = $('.dropdown__menu').closest('.dropdown').children('.nav__link:first-child');
+
+    navLink.on('click', function(e) {
+      e.preventDefault();
+
+      if($(this).hasClass('active')){
+        $(this).removeClass('active')
+        $(this).parent().find('.dropdown__menu').removeClass('show');
+      } else {
+        $(this).addClass('active')
+        $(this).parent().find('.dropdown__menu').addClass('show');
+      }
+    })
+
+    let toggleSidebar = $('.navbar__sidebar-toggler');
+    let sidebar = $('#sidebar');
+
+
+    toggleSidebar.on('click', function(e) {
+      e.preventDefault();
+
+      if($(this).hasClass('collapsed')){
+        $(this).removeClass('collapsed')
+        sidebar.removeClass('sidebar--collapsed');
+      } else {
+        $(this).addClass('collapsed')
+        sidebar.addClass('sidebar--collapsed');
+      }
+    })
+  });
+}(window, jQuery));
